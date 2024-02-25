2b369edd3cb3ed7bff8bcf0c111844973040137d
WarDaddyPershing <160628792+WarDaddyPershing@users.noreply.github.com>
2024-02-25 13:25:27 +0800

    02/25/2024
    This is an upload on the latest changes made on 02/25/2024

---
+++ Activity2.m
@@ -0,0 +1,19 @@
+clc
+clear all
+close all
+
+syms t s
+
+%Solve for the iverse laplace transform of the following
+
+F1 = ((1)/(s*(s^2 + 2*s + 2)));
+F2 = ((5)*(s + 2))/(((s^2)*((s + 1)*(s + 3))));
+F3 = ((s^4 + 2*s^3 + 3*s^2 + 4*s + 5)/(s*(s + 1)));
+
+f1 = ilaplace(F1)
+f2 = ilaplace(F2)
+f3 = ilaplace(F3)
+
+pretty(f1)
+pretty(f2)
+pretty(f3)
